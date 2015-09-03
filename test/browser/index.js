206ce09e92b4c585585a83319c7cf28676321b4e
Geoff Cox <redgeoff@gmail.com>
2015-09-03 04:16:37 +0000

    test(travis): failing phantomjs test

--- test/browser/index.js
+++ test/browser/index.js
@@ -103,7 +103,7 @@ describe('browser-tmp', function () {
           },
           '4': {
             $id: '4',
-            name: 'green'
+            name: 'green1'
           }
         }
       };
