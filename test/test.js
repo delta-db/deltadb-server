ab365e65025bf9e6f2b859d195204929fe0330e0
Geoff Cox <redgeoff@gmail.com>
2015-10-11 21:38:50 +0000

    restore testdb tests

--- test/test.js
+++ test/test.js
@@ -15,17 +15,25 @@ describe('deltadb', function () {
   utils.setUp(this);
 
   before(function () {
-// TODO: restore
+    // Create the db and only once for all the tests
+    var db = new Partitioner('testdb');
+    return db.createDatabase().then(function () {
+      return db.closeDatabase(); // close as beforeEach will connect
+    });
+  });
+
+//   before(function () {
+// // TODO: restore
 //     // Create the db and only once for all the tests
 //     var db = new Partitioner('testdb');
 //     return db.connect().then(function () {
 //       return db.closeDatabase(); // close as beforeEach will connect
 //     }).catch(function (err) {
-// console.log('err=', err);
-// console.log('huh, db exists!!!');
-// process.exit(1);
+//     console.log('err=', err);
+//     console.log('huh, db exists!!!');
+//     process.exit(1);
 //       if (err instanceof DBMissingError) {
-// console.log('&&&&&&&missing so create');
+//     console.log('&&&&&&&missing so create');
 //         return db.createDatabase().then(function () {
 //           return db.closeDatabase(); // close as beforeEach will connect
 //         });
@@ -33,15 +41,14 @@ describe('deltadb', function () {
 //         throw err;
 //       }
 //     });
-  });
+//   });
 
   after(function () {
-// TODO: restore
-    // // Destroy the db after all the tests
-    // var db = new Partitioner('testdb');
-    // return db.connect().then(function () {
-    //   return db.destroyDatabase();
-    // });
+    // Destroy the db after all the tests
+    var db = new Partitioner('testdb');
+    return db.connect().then(function () {
+      return db.destroyDatabase();
+    });
   });
 
   require('./spec');
