3178efdbe966cc12b64e48babe6b5551293c5992
Geoff Cox <redgeoff@gmail.com>
2015-10-18 23:40:09 +0000

    dbg(travis): path.posix.relative issue

--- test/browser/index.js
+++ test/browser/index.js
@@ -2,11 +2,11 @@
 
 describe('browser', function () {
 
-  // require('./adapters');
+  require('./adapters');
   require('../spec/orm/nosql/common');
-  // require('../spec/utils/utils');
-  //
-  // // TODO: also test entire client!
-  // require('../spec/client/utils');
+  require('../spec/utils/utils');
+
+  // TODO: also test entire client!
+  require('../spec/client/utils');
 
 });
