d72bdfee6d166682b932b9737ec7e7803d5680e3
Geoff Cox <redgeoff@gmail.com>
2015-10-21 02:01:11 +0000

    fix(adapter): forgot fake

--- test/spec/client/adapter.js
+++ test/spec/client/adapter.js
@@ -63,6 +63,8 @@ describe('adapter', function () {
   });
 
   it('should disconnect when destroying', function () {
+
+    fakeResolveAfterDatabaseCreated();
     fakeResolveAfterDatabaseDestroyed();
 
     var db = client.db({
