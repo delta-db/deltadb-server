e624b6b823f4c6123938a5b75f7968de8a8f6263
Geoff Cox <redgeoff@gmail.com>
2015-10-27 14:29:37 +0000

    test(e2e): remove dep on idb

--- test/e2e/e2e.js
+++ test/e2e/e2e.js
@@ -1,28 +1,6 @@
 'use strict';
 
-/* global before */
-
-var idbUtils = require('../../scripts/orm/nosql/adapters/indexeddb/utils'),
-  IDBAdapter = require('../../scripts/orm/nosql/adapters/indexeddb');
-
 describe('e2e', function () {
-
-  before(function () {
-    if (global.window && idbUtils.indexedDB()) { // using IndexedDB?
-      // Our tests will occassionally break when we change the code so we make sure that we remove
-      // any test IndexedDB's so that we don't have to remove them manually in the browser.
-      var idb = new IDBAdapter();
-      var system = idb.db({
-        db: '$system'
-      });
-      var mydb = idb.db({
-        db: 'mydb'
-      });
-      return system.destroy().then(function () {
-        return mydb.destroy();
-      });
-    }
-  });
 
   require('./basic');
 
