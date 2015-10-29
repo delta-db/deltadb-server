da22118edd54e6c84eadc61a27c24c65c2d39608
Geoff Cox <redgeoff@gmail.com>
2015-10-29 03:27:18 +0000

    test(node): 100% coverage

--- scripts/client/db.js
+++ scripts/client/db.js
@@ -445,15 +445,11 @@ DB.prototype._connect = function () {
 
 DB.prototype._disconnect = function () {
   var self = this;
-  if (this._socket) { // is there already a connection?
-    return self._ready().then(function () {
-      var promise = utils.once(self, 'disconnect');
-      self._socket.disconnect();
-      return promise;
-    });
-  } else {
-    return Promise.resolve();
-  }
+  return self._ready().then(function () {
+    var promise = utils.once(self, 'disconnect');
+    self._socket.disconnect();
+    return promise;
+  });
 };
 
 DB.prototype._connectWhenReady = function () {
--- test/spec/orm/sql/adapters/postgres/index.js
+++ test/spec/orm/sql/adapters/postgres/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var SQL = require('../../../../../../scripts/orm/sql/adapters/postgres'),
-  commonUtils = require('../../../../../common-utils');
+  commonUtils = require('../../../../../common-utils'),
+  connections = require('../../../../../../scripts/orm/sql/adapters/postgres/connections');
 
 describe('postgres', function () {
   var sql = new SQL();
@@ -11,5 +12,10 @@ describe('postgres', function () {
     return commonUtils.shouldNonPromiseThrow(function () {
       sql._processConnectError(err);
     }, err);
+  });
+
+  it('should prevent race conditions when unregistering', function () {
+    // This fn would throw an error if it was unable to handle the race conditions
+    return connections._unregister(1, 'db', 'host', 'username', 'password', 8080);
   });
 });
