a39fd5621bba0d095c0115c00754b4a7c9d32718
Geoff Cox <redgeoff@gmail.com>
2015-11-20 02:42:36 +0000

    fix(postgres): handle close races

--- test/spec/orm/sql/adapters/postgres/index.js
+++ test/spec/orm/sql/adapters/postgres/index.js
@@ -2,10 +2,15 @@
 
 var SQL = require('../../../../../../scripts/orm/sql/adapters/postgres'),
   commonUtils = require('../../../../../common-utils'),
-  connections = require('../../../../../../scripts/orm/sql/adapters/postgres/connections');
+  connections = require('../../../../../../scripts/orm/sql/adapters/postgres/connections'),
+  SocketClosedError = require('../../../../../../scripts/orm/sql/common/socket-closed-error');
 
 describe('postgres', function () {
-  var sql = new SQL();
+  var sql = null;
+
+  beforeEach(function () {
+    sql = new SQL();
+  });
 
   it('should throw errors when connecting', function () {
     var err = new Error();
@@ -17,5 +22,14 @@ describe('postgres', function () {
   it('should prevent race conditions when unregistering', function () {
     // This fn would throw an error if it was unable to handle the race conditions
     return connections._unregister(1, 'db', 'host', 'username', 'password', 8080);
+  });
+
+  it('should handle race conditions when querying', function () {
+    // Fake
+    sql._connection = null;
+
+    return commonUtils.shouldThrow(function () {
+      return sql._query();
+    }, new SocketClosedError());
   });
 });
