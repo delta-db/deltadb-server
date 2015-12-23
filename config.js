f99101da38d668fe7b59b617a50ef0a7b18a00c0
Geoff Cox <redgeoff@gmail.com>
2015-12-23 15:31:47 +0000

    feat(config): use default config

--- config.js
+++ config.js
@@ -1,9 +1,20 @@
 'use strict';
 
-var vals = require('./config.json');
+var fs = require('fs');
 
 var Config = function () {
-  this.vals = vals;
+  this._loadVals(fs);
+};
+
+// We define _fs so that we can test this fn
+Config.prototype._loadVals = function (_fs) {
+  var json = null;
+  try {
+    json = _fs.readFileSync('config.json');
+  } catch (err) {
+    json = _fs.readFileSync('config-default.json');
+  }
+  this.vals = JSON.parse(json);
 };
 
 Config.prototype.url = function () {
